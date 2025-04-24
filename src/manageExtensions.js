cd6d110cbcbf4f6065f56cd41598219bbba0f493
wendyhamel <whamel@it4web.nl>
2025-04-24 12:40:44 +0200

    Use system darkmode when the button is not toggled. Added darkmode button with icons; added font; added project theme colors.

--- src/manageExtensions.js
+++ src/manageExtensions.js
@@ -1,16 +1,14 @@
 window.extensionsJS = function () {
 	return {
-		theme: Alpine.$persist(localStorage.getItem(this.darkMode) || 'system'),
-		isDarkMode: Alpine.$persist(window.matchMedia('(prefers-color-scheme: dark)').matches),
+		theme: Alpine.$persist(localStorage.getItem(this.isDarkMode) || 'system'),
+		isDarkMode: window.matchMedia('(prefers-color-scheme: dark)').matches,
 
 		toggleDarkMode () {
 			this.isDarkMode = !this.isDarkMode
-			if (this.theme === 'system') {
-				if (this.isDarkMode) {
-					this.theme = 'dark'
-				} else {
-					this.theme = 'light'
-				}
+			if (this.isDarkMode) {
+				this.theme = 'dark'
+			} else {
+				this.theme = 'light'
 			}
 		},
 
